8e7a643f5302e9d1b1c6322d90bce930501b3aa5
Laura Adao <laura.adao@ens.uvsq.fr>
2025-03-02 21:26:27 +0100

    rajout moyens

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,10 @@
 # but du jeu battre le croupier sans dépasser 21, si 21 dépassé = on perd (le joueur "brûle")
 # valeur des cartes : de 2 à 9, du 10 au roi = 10 points et l'as = 1 ou 11 points (choix du joueur)
+#   == liste, dictionnaire soit un conteneur associant les cartes à des points
 # déroulement : première carte distribué visible pour j et c, deuxième carte visible pour le j et non le c
 #           soit blackjack = victoire 
 #           sinon plusieurs choix : "carte" qui lui rajoute une carte, conserver sa mise, 
-#doubler sa mise seulement si on rajoute une carte ensuite ou abandonner (à voir)   
+#doubler sa mise seulement si on rajoute une carte ensuite ou abandonner (à voir) 
+#   == itérations  
 #pour le c : il tire des cartes jusqu'à atteindre au moins 17 ou plus (d'autres conditions)
+# + interface graphique, tenter les mises 
