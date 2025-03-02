d801f168366da20d4664029f91b4f63c8ff2d292
Laura Adao <laura.adao@ens.uvsq.fr>
2025-03-02 21:23:07 +0100

    règles/objectifs

---
+++ blackjack.py
@@ -0,0 +1,7 @@
+# but du jeu battre le croupier sans dépasser 21, si 21 dépassé = on perd (le joueur "brûle")
+# valeur des cartes : de 2 à 9, du 10 au roi = 10 points et l'as = 1 ou 11 points (choix du joueur)
+# déroulement : première carte distribué visible pour j et c, deuxième carte visible pour le j et non le c
+#           soit blackjack = victoire 
+#           sinon plusieurs choix : "carte" qui lui rajoute une carte, conserver sa mise, 
+#doubler sa mise seulement si on rajoute une carte ensuite ou abandonner (à voir)   
+#pour le c : il tire des cartes jusqu'à atteindre au moins 17 ou plus (d'autres conditions)
