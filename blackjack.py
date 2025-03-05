602dc75086f36b85b578d4fb468bf71d49f16759
Sophie <sam.sophie.quennesson@gmail.com>
2025-03-05 15:32:23 +0100

    corrections+mises

--- blackjack.py
+++ blackjack.py
@@ -8,7 +8,10 @@
 #   == itérations  
 #pour le c : il tire des cartes jusqu'à atteindre au moins 17 ou plus (d'autres conditions)
 # + interface graphique, tenter les mises 
-import random
+#règles possibles à ajouter: Abandonner, Doubler, Splitter 
+#règles:https://www.mundijeux.fr/multijoueur/blackjack/regles/#:~:text=Splitter%20.,possible%20de%20faire%20%E2%80%9CBlackjack%E2%80%9D.
+
+import random as rd
 import tkinter as tk
 
 rangs={"As":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "Valet":10, "Dame":10, "Roi":10}
@@ -19,7 +22,7 @@ def carte(paquet, n):
     """génère un nombre n de cartes du paquet"""
     main=[]
     for i in range (n):
-        carte=random.choice(paquet)
+        carte=rd.choice(paquet)
         main.append(carte)
     return(main)
 
@@ -35,49 +38,63 @@ def valeur(main):
     while valeur>21 and nb_as!=0:
         valeur-=10
         nb_as-=1
-    return(float(valeur))
+    return(valeur)
 
 
-def partie():
+def partie(jeton):
 
     main_joueur=carte(paquet,2)
     main_croupier=carte(paquet,1)
 
     print(f"Votre main : {main_joueur} (Valeur: {valeur(main_joueur)})")
-    print(f"Carte visible du croupier : {main_croupier}")
+    print(f"Carte visible du croupier : {main_croupier} (Valeur: {valeur(main_croupier)})")
+
+    mise=int(input(f"Votre solde de jeton est de {jeton}. Combien voulez-vous miser pour cette partie? "))
+    while mise<=0 or mise>jeton:
+        print("Votre mise n'est pas valide, veuillez entrer un montant valide!")
+        mise=int(input("Quelle est votre nouvelle mise? "))
 
     if valeur(main_joueur)==21:
-        return("Blackjack!")
+        print("Blackjack!")
+        return(jeton+int(mise*1.5))
     
     while valeur(main_joueur)<21:
-        choix=input("Voulez-vous tirer ou rester?")
+        choix=input("Voulez-vous tirer ou rester? ")
         if choix=="Tirer":
             main_joueur.extend(carte(paquet,1))
             print(f"Nouvelle main : {main_joueur} (Valeur: {valeur(main_joueur)})")
-            while valeur(main_croupier)<16:
-                main_croupier.extend(carte(paquet,1))
         elif choix=="Rester":
-            break
+            pass
 
         while valeur(main_croupier)<16:
             main_croupier.extend(carte(paquet,1))
         print(f"Main du croupier : {main_croupier} (Valeur: {valeur(main_croupier)})")
 
         if valeur(main_joueur)>21:
-            return("Dust!")
+            print("Dust!")
+            return(jeton-mise)
         elif valeur(main_croupier)>21:
-            return("Gagné!")
+            print("Gagné!")
+            return(jeton+mise)
         elif valeur(main_croupier)>valeur(main_joueur):
-            return("Perdu!")
+            print("Perdu!")
+            return(jeton-mise)
         elif valeur(main_joueur)>valeur(main_croupier):
-            return("Gagné!")
+            print("Gagné!")
+            return(jeton+mise)
         elif valeur(main_joueur)==valeur(main_croupier):
-            return("Egalité!")
+            print("Egalité!")
+            return(jeton)
 
 def jouer():
-    while True:
-        print(partie())
-        nouvelle_partie=input("Voulez-vous jouer encore un fois?")
+    print("Bienvenue au Blackjack!")
+    jeton=200
+    while jeton>0:
+        jeton=partie(jeton)
+        if jeton<=0:
+            print("Vous n'avez plus de jetons!")
+            break
+        nouvelle_partie=input("Voulez-vous jouer encore un fois? ")
         if nouvelle_partie!="Oui":
             print("Merci d'avoir joué!")
             break
