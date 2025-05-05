3cb4f74b75c889549009f87d664fafce460ffde8
Laura A <laura.adao@ens.uvsq.fr>
2025-05-05 18:23:21 +0200

    code soigné

--- blackjack_v1.py
+++ blackjack_v1.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 import random as rd
 
-# ------- Fonctions ---------------
+
+#_____________Initialisation_____________
+
+
+
 rangs = {"As": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
          "7": 7, "8": 8, "9": 9, "10": 10, "Valet": 10, "Dame": 10, "Roi": 10}
 couleurs = ['♠', '♥', '♦', '♣']
@@ -10,15 +14,16 @@ paquet = [f"{rang} de {couleur}" for rang in rangs for couleur in couleurs]
 
 mise_utilisateur = 0
 jeton = 100
+mise = 0
+boutons = []
+
+#tour_actuel = 0
 
 game_over = False
 doubler_mise = False
 abandon = False
 variable_splitter = False
-mise = 0
-boutons = []
-#tour_actuel = 0
-#label_choix = None
+
 bouton_tirer = None
 bouton_rester = None
 bouton_splitter = None
@@ -27,7 +32,12 @@ bouton_abandonner = None
 bouton_nv_manche = None
 bouton_nv_partie = None
 
+
+
+#_____________Fonctions partie_____________
+
 def carte(paquet: list, n: int) -> list:
+    """Retourne la main du joueur/croupier"""
     main = []
     for _ in range(n):
         carte = rd.choice(paquet)
@@ -36,9 +46,11 @@ def carte(paquet: list, n: int) -> list:
     return main
 
 def quitter():
+    """Quitte la fenetre"""
     racine.destroy()
 
 def play():
+    """Restaure la fenêtre et lance le jeu"""
     frame_cadre_gris.grid_forget()
     frame_boutons.grid_forget()
     label_credits.grid_forget()
@@ -46,6 +58,7 @@ def play():
     commencer_partie()
 
 def nouvelle_partie():
+    """Permet de recommencer une partie après la perte de tout les jetons, qui sont mis à jour"""
     global mise,jeton, bouton_quit, label_fin_de_jeu, bouton_nv_partie
     jeton = 100
     mise = 0
@@ -55,18 +68,18 @@ def nouvelle_partie():
     commencer_partie()
 
 def commencer_partie():
+    """Démarre la partie de jeu, introduction de la mise à choisir"""
     global label_jetons, label_mise, bouton_valider, label_fin_de_jeu, bouton_quit, bouton_nv_partie
 
     if jeton <= 0:
         label_fin_de_jeu = tk.Label(racine, text="Vous n'avez plus de jeton, le jeu est terminé", font=("helvetica", 14))
         label_fin_de_jeu.grid(row=1, column=0, columnspan=2, pady=20)
-        bouton_quit = tk.Button(racine, text="Quit", font=("Arial", 20), width=20, command=quitter)
+        bouton_quit = tk.Button(racine, text="Quitter", font=("Arial", 20), width=20, command=quitter)
         bouton_quit.grid(row=2, column=0, columnspan=2, pady=10)
         bouton_nv_partie = tk.Button(racine, text="Nouvelle Partie", command=nouvelle_partie, fg="white", bg="gray22")
         bouton_nv_partie.grid(row=3,column=0,columnspan=3)
         return
 
-    # Cadre principal de la partie
     cadre_mise = tk.Frame(racine, bg="gray22", bd=2, padx=10, pady=10)
     cadre_mise.grid(row=1, column=1, columnspan=2, pady=(20, 10), sticky="n")
 
@@ -87,16 +100,17 @@ def commencer_partie():
     bouton_valider.grid(row=5, column=0, columnspan=3, pady=10)
 
 def ajouter_mise(montant):
+    """Ajoute un montant à la mise"""
     global mise
     mise += montant
     label_mise.config(text=f"Mise actuelle : {mise} €")
 
 def valider_mise():
+   """Conditions pour valider la mise """
    global boutons, jeton, mise
-   #grise boutons_mise
    for bouton_mise in boutons:
        bouton_mise.config(state="disabled")
-   if mise >0 and mise <= jeton :
+   if mise > 0 and mise <= jeton :
        mise_soumise()
    else:
        mise = 0
@@ -104,22 +118,23 @@ def valider_mise():
        quit()
 
 def mise_soumise():
+    """Valide la mise """
     global mise_utilisateur, jeton, bouton_valider , label_mise, mise
-
     mise_utilisateur = mise
     jeton -= mise_utilisateur
     label_jetons.config(text=f"Vous avez {jeton} jetons.", font=("helvetica",14))
     label_mise.config(text=f"Mise acceptée : {mise_utilisateur}")
     bouton_valider.config(state="disabled")
-    #fonction pour ditribuer les cartes
+    
     mains_joueurs()
-    print(f"Mise acceptée : {mise_utilisateur} Jetons : {jeton}")   
+    #print(f"Mise acceptée : {mise_utilisateur} Jetons : {jeton}")   
 
 def mains_joueurs():
+    """Distribue les cartes"""
     global main_joueur, main_croupier, paquet, label_joueur, label_croupier, cadre_joueurs, cadre_joueur, cadre_resultat
  
-    main_joueur=carte(paquet,2) #Création de la main initiale du joueur.
-    main_croupier=carte(paquet,1) #Création de la main initiale du croupier.
+    main_joueur=carte(paquet,2) 
+    main_croupier=carte(paquet,1) 
 
     if valeur(main_joueur)==21:
         blackjack()
@@ -137,7 +152,6 @@ def mains_joueurs():
                               fg="white",font=("Arial", 10))
     label_croupier.grid(row=2, column=0, columnspan=2)
 
-    #affichage stylisé de la main du joueur:
     cadre_joueur = tk.Frame(cadre_joueurs, bg="gray22", bd=2, padx=10, pady=10)
     cadre_joueur.grid(row=2,column=0,columnspan=2)
     
@@ -146,18 +160,16 @@ def mains_joueurs():
     choix()
 
 def affichage_main_joueur():
+    """Affichage stylysé des cartes"""
     global cadre_joueur, couleurs_cartes
 
-    # supression => pas empiler
     for widget in cadre_joueur.winfo_children():
         widget.destroy()
 
-    # titre des cartes
     titre_label = tk.Label(cadre_joueur, text=f"Votre main = ({valeur(main_joueur)})",
                            bg="gray22", fg="white", font=("Arial", 10))
     titre_label.grid(row=0, column=0, columnspan=len(main_joueur), pady=(0, 10))
 
-    # Affichage stylisé des cartes
     for i, carte_txt in enumerate(main_joueur):
         rang, couleur = carte_txt.split(" de ")
         symbole = couleur.strip()[0]  # ♠ ♥ ♦ ♣
@@ -171,9 +183,9 @@ def affichage_main_joueur():
         carte_label.grid(row=1, column=i, padx=5)
 
 def valeur(main:list)->int:
-    """renvoie la valeur des cartes dans une main"""
+    """Renvoie la valeur des cartes dans une main"""
     valeur = 0
-    nb_as = 0 #La valeur des as est gérée de telle sorte que lorsque la valeur de la main dépasse 21 les as comptent 1.
+    nb_as = 0 
     for carte in main:
         rang = carte.split(" de ")[0] #Permet de stocker dans la variable uniquement le rang de la carte, soit la clé dans le dictionnaire rangs.
         valeur += rangs[rang]
@@ -185,10 +197,9 @@ def valeur(main:list)->int:
     return(valeur)
 
 def choix():
-    global bouton_tirer, bouton_rester, main_joueur, mise_utilisateur, jeton, bouton_doubler, bouton_splitter, bouton_abandonner #label_choix
+    """Choix du jeu"""
+    global bouton_tirer, bouton_rester, main_joueur, mise_utilisateur, jeton, bouton_doubler, bouton_splitter, bouton_abandonner
 
-    #if label_choix:
-    #    label_choix.grid_forget()
     if bouton_tirer and bouton_tirer.winfo_exists(): 
         bouton_tirer.grid_forget()
         bouton_tirer = None
@@ -204,9 +215,6 @@ def choix():
     if bouton_abandonner and bouton_abandonner.winfo_exists():
         bouton_abandonner.grid_forget()
         bouton_abandonner = None
-
-    #label_choix = tk.Label(cadre_joueurs, text="Voulez-vous tirer ou rester? ")
-    #label_choix.grid(row=3, column=0, columnspan= 2)
 
     bouton_tirer = tk.Button(cadre_joueur, text="Tirer", command=tirer)
     bouton_tirer.grid(row=4,column=1)
@@ -226,8 +234,8 @@ def choix():
             bouton_splitter.grid(row=6, column=0, columnspan= 2)
 
 def tirer():
-    global main_joueur, game_over, paquet, doubler_mise, label_main_joueur_1, label_main_joueur_2, variable_splitter, main_actuelle, main_joueur_1, main_joueur_2
     """Rajoute une carte au joueur"""
+    global main_joueur, game_over, paquet, doubler_mise, label_main_joueur_1, label_main_joueur_2, variable_splitter, main_actuelle, main_joueur_1, main_joueur_2
     if doubler_mise:
         main_joueur.extend(carte(paquet, 1))
         affichage_main_joueur()
@@ -300,17 +308,13 @@ def blackjack():
 
     bouton_tirer.grid_forget()
     bouton_rester.grid_forget()
-    print("blackjack !")
 
     bouton_nv_manche = tk.Button(racine, text="Nouvelle Manche", command=nouvelle_manche, fg="white", bg="gray22")
     bouton_nv_manche.grid(row=3,column=0,columnspan=3)
 
 def resultat():
     """Renvoie les résultats du tour de jeu"""
-    global jeton, abandon, bouton_nv_manche #, label_choix
-
-    #label_choix.grid_forget()
-
+    global jeton, abandon, bouton_nv_manche
     if abandon:
         message(f"Vous abandonnez, vous perdez la moitié de votre mise.")
         jeton+=mise_utilisateur // 2
@@ -387,17 +391,21 @@ def nouvelle_manche():
 
     commencer_partie()
 
-#---------Abandonner------------
+#_____________Fonctions bonus_____________
+
+#____Abandonner
 
 def abandonner():
+    """Permet d'abandonner la manche"""
     global abandon, game_over
     abandon=True
     game_over=True
     resultat()
 
-#---------Doubler-------------
+#____Doubler____
 
 def doubler():
+    """Double la mise du joueur, obligation de tirer"""
     global mise_utilisateur, jeton, doubler_mise
     doubler_mise = True
     mise_utilisateur *= 2
@@ -405,10 +413,13 @@ def doubler():
     label_jetons.config(text=f"Vous avez {jeton-mise_utilisateur} jetons.")
     tirer()
 
-#---------Splitter-------------
+#____Splitter____
 
 def splitter():
+    """Coupe la main du joueur en 2"""
     global main_joueur, main_joueur_1, main_joueur_2, variable_splitter, main_actuelle, jeton, mise_utilisateur
+
+    racine.geometry('800x500')
 
     variable_splitter = True
     main_actuelle = 1
@@ -424,6 +435,7 @@ def splitter():
     jouer_main_split()
     
 def jouer_main_split():
+    """Jouer les deux mains"""
     global main_actuelle, main_joueur_1, main_joueur_2, label_main_joueur_1, label_main_joueur_2, cadre_joueur
 
     cadre_split = tk.Frame(racine, bg="gray22",bd=2, padx=10, pady=10)
@@ -431,14 +443,15 @@ def jouer_main_split():
     
     if main_actuelle == 1:
         label_main_joueur_1 = tk.Label(cadre_split, text=f"Votre première main : {main_joueur_1}, (Valeur: {valeur(main_joueur_1)})",bg="gray22",fg="white",font=("Arial", 10))
-        label_main_joueur_1.grid()
+        label_main_joueur_1.grid(row=0, column=0)
     elif main_actuelle == 2:
         label_main_joueur_2 = tk.Label(cadre_split, text=f"Votre seconde main : {main_joueur_2}, (Valeur: {valeur(main_joueur_2)})",bg="gray22",fg="white",font=("Arial", 10))
-        label_main_joueur_2.grid()
+        label_main_joueur_2.grid(row=0, column=1)
     
     choix()
 
 def resultat_split():
+    """"Affiche les résultats en cas de split"""
     global jeton
 
     for resultat in [valeur(main_joueur_1), valeur(main_joueur_2)]:
@@ -462,16 +475,20 @@ def resultat_split():
             message("Egalité! Vous récuperez votre mise")
             jeton+=mise_utilisateur
     
-    bouton_nv_manche = tk.Button(racine, text="Nouvelle Manche", command=nouvelle_manche)
-    bouton_nv_manche.grid(row=3,column=0,columnspan=3)
+    bouton_nv_manche = tk.Button(racine, text="Nouvelle Manche", command=nouvelle_manche, fg="white", bg="gray22")
+    bouton_nv_manche.grid(row=8,column=0,columnspan=3)
 
-# ------- Fenêtre principale -------
+
+
+#_____________Fenêtre principale_____________
+
+
 racine = tk.Tk()
 racine.title("Blackjack")
-racine.geometry('800x500')
+racine.geometry('700x450')
 racine.configure(bg="aquamarine4")
 
-# Ligne 0 : Titre
+
 frame_cadre_gris = tk.Frame(racine, bg="gray22", bd=2, padx=10, pady=10)
 frame_cadre_gris.grid(row=0, column=0, columnspan=2, pady=10, sticky="n")
 
@@ -482,11 +499,11 @@ label_demarrage.grid(row=0, column=0)
 label_decoration = tk.Label(racine, bg="aquamarine4", text="♠ ♥ ♦ ♣", fg="gray22",font = ("Courier", "30", "bold"))
 label_decoration.grid(row=1,column=0,columnspan=2)
 
-# artificielle
+
 racine.grid_columnconfigure(0, weight=1)
 racine.grid_columnconfigure(1, weight=1)
 
-# Ligne 1 : Boutons "Play" et "Quit"
+
 frame_boutons = tk.Frame(racine, bg="aquamarine4")
 frame_boutons.grid(row=2, column=0, columnspan=2)
 
@@ -496,8 +513,10 @@ bouton_demarrer.grid(row=0, column=0, padx=10, pady=5)
 bouton_quitter = tk.Button(frame_boutons, text="Quit", font=("Arial", 20, "bold"), width=20, fg="white", bg="gray22", command=quit)
 bouton_quitter.grid(row=1, column=0, padx=10, pady=5)
 
-# Ligne 2 : Crédits
+
 label_credits = tk.Label(racine, text="Sophie, Laura, Fanilo, Yacine", font=("Arial", 10), bg="aquamarine4", fg="white")
 label_credits.grid(row=3, column=0, columnspan=2, pady=20, sticky="s")
+
+
 
 racine.mainloop()
